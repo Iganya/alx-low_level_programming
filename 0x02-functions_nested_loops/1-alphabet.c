d0dbe5510da62bf894840b43ccaad8d4c7591195
Iganya <matthewiganga@gmail.com>
2022-09-13 15:00:25 -0700

    Print lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+
+ * print alphabet - print alphabet lowercase
+
+ * Return: Always 0.
+
+ */
+
+void print_alphabet(void)
+
+{
+
+    char alpha;
+
+    for (alpha = 'a'; alpha <= 'z'; alpha++)
+
+        {
+
+        _putchar(alpha);
+
+        }
+
+    _putchar('\n');
+
+
+
+}
