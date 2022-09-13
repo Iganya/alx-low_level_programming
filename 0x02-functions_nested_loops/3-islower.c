18beae89918df6135a92977fdf293cac6b69808a
Iganya <matthewiganga@gmail.com>
2022-09-13 15:24:42 -0700

    checking for lower c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _islower - Sheck for lowe c
+* @c: contains value to be compared
+* Return: 0 or 1 if condition is met
+*/
+int _islower(int c)
+{
+
+if (c > 'a' && c < 'z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
