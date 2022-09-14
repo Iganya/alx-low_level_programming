11952bb7698e2acce135488117326e389ae1f03c
Iganya <matthewiganga@gmail.com>
2022-09-14 13:48:26 -0700

    Prints all natural Numbers

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,7 +7,7 @@ void times_table(void)
 {
 int a, b, c;
 
-for (a = 0; a <= 9; a++)
+	for (a = 0; a <= 9; a++)
 	{
 
 		for (b = 0; b <= 9; b++)
@@ -19,13 +19,11 @@ for (a = 0; a <= 9; a++)
 				{
 					_putchar ('0');
 				}
-				
 				if (b != 0)
 				{
 					_putchar (' ');
 					_putchar ((c % 10) + '0');
 				}
-				
 				if (b < 9)
 				{
 					_putchar(',');
@@ -36,7 +34,6 @@ for (a = 0; a <= 9; a++)
 			{
 				_putchar ((c / 10) + '0');
 				_putchar ((c % 10) + '0');
-				
 				if (b < 9)
 				{
 					_putchar(',');
