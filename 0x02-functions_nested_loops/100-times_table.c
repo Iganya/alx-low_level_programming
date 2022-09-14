0bf191495a381831e55a508600fde82db731cacf
Iganya <matthewiganga@gmail.com>
2022-09-14 14:37:15 -0700

    List naturall numbers that are multiple of 3 and 10

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,10 +7,9 @@
 void print_times_table(int n)
 {
 	int a = 0, rep, b;
-	
+
 	if (n < 0 || n > 15)
 	return;
-	
 	while (a <= n)
 	{
 		for (b = 0; b <= n; b++)
@@ -18,7 +17,6 @@ void print_times_table(int n)
 			rep = a * b;
 			if (b == 0)
 			_putchar('0' + rep);
-			
 			else if (rep < 10)
 			{
 				_putchar(' ');
