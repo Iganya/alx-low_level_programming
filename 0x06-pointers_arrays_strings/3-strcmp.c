8752d6d78f8a0ecd4f9093d51beff43ddb10612b
Iganya <matthewiganga@gmail.com>
2022-09-21 14:12:06 -0700

    Change lowercase to upper case

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,6 @@
 * @s2: string 2
 * Return: 0 if matching, and ns1 - ns2 if not matching
 */
-
 int _strcmp(char *s1, char *s2)
 {
 int i;
@@ -14,7 +13,6 @@ int i;
 * We have reached the end of S1 OR we have reached the end of S2
 * 
 */
-
 for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 {
 	if (s1[i] != s2[i])
@@ -23,4 +21,5 @@ for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	}
 }
 return (0);
+
 }
